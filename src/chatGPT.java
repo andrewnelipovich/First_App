bdadc0c0dd140380d9896bb78afae832f90315a6
Andrew Nelipovich <vq35dett@mail.ru>
2023-10-22 15:26:53 +0200

    chatGPT class added

---
+++ src/chatGPT.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class chatGPT {
+        public static void main(String[] args) {
+            // Create a Scanner object to read user input
+            Scanner scanner = new Scanner(System.in);
+
+            // Prompt the user for input
+            System.out.print("Enter a value: ");
+
+            // Read the user's input as a string
+            String userInput = scanner.nextLine();
+
+            // Close the scanner
+            scanner.close();
+
+            // Print the user's input
+            System.out.println("Your input: " + userInput);
+        }
+    }
