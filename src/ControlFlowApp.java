8f898ce54f4cc89925cea03c5fd018dd5f7c2a72
andrewnelipovich <andrew.nelipovich@gmail.com>
2023-11-20 19:27:21 +0100

    Lesson 42 (Control flow)

---
+++ src/ControlFlowApp.java
@@ -0,0 +1,39 @@
+public class ControlFlowApp {
+    public static void main(String[] args) {
+
+        boolean isDamaged = false;
+        String carColor = "Green";
+        char energyEfficiencyCategory = 'a';
+
+        if (isDamaged) {
+            System.out.println("Damaged car");
+        }
+        else {
+            System.out.println("Undamaged");
+        }
+
+        if (carColor.equals("red")) {
+            System.out.println("Car is red");
+        } else if (carColor.equals("green")) {
+            System.out.println("Car is green");
+        } else if (carColor.equals("blue")) {
+            System.out.println("Car is blue");
+        } else {
+            System.out.println("Color unknown");
+        }
+
+        switch (energyEfficiencyCategory) {
+            case 'A':
+            case 'B':
+                System.out.println("Low energy");
+                break;
+            case 'G':
+                System.out.println("High energy");
+                break;
+            default:
+                System.out.println("Consumption unknown");
+        }
+
+
+    }
+}
---
+++ src/MillionaireHomeworkApp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MillionaireHomeworkApp {
+    public static void main(String[] args) {
+
+        char correctAnswer = 'c';
+
+        System.out.println("Who invented light bolb? \na.Him \nb.Her \nc.Thomas \nd.They");
+
+        Scanner inputText = new Scanner(System.in);
+
+        System.out.println("Enter your answer:");
+
+        char userInput = inputText.next().charAt(0);
+
+        if (userInput == correctAnswer) {
+            System.out.println("That's right!");
+        } else {
+            System.out.println("Wrong answer");
+        }
+        inputText.close();
+    }
+}
