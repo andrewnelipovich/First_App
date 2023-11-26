562054a26127df76646594883fa20dd3f8e7c3b4
andrewnelipovich <andrew.nelipovich@gmail.com>
2023-11-26 17:10:33 +0100

    Lesson 50 (Beginner - Final Practice)

---
+++ src/FinalPracticeApp.java
@@ -0,0 +1,18 @@
+public class FinalPracticeApp {
+    public static void main(String[] args) {
+        String name = "Michael Fassbender";
+        String dateOfBirth = "April 2, 1977";
+
+        String[] movieTitles = new String[3];
+        movieTitles[0] = "Shame";
+        movieTitles[1] = "Prometheus";
+        movieTitles[2] = "X-Men: First Class";
+
+        double[] movieRatings = new double[3];
+        movieRatings[0] = 7.2;
+        movieRatings[1] = 7.0;
+        movieRatings[2] = 7.7;
+
+
+    }
+}
