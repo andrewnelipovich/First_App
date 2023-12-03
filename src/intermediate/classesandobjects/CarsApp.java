f99b6033c266795cafa528c1f18d7412308650f4
andrewnelipovich <andrew.nelipovich@gmail.com>
2023-12-03 15:29:25 +0100

    Lesson 58 (Classes and objects)

---
+++ src/intermediate/classesandobjects/CarsApp.java
@@ -0,0 +1,14 @@
+package intermediate.classesandobjects;
+
+public class CarsApp {
+    public static void main(String[] args) {
+//        DodgeChallenger dodgeChallenger = new DodgeChallenger();
+//        dodgeChallenger.startTheEngine();
+//        DodgeChallenger.getDescription();
+//        System.out.println();
+
+        DodgeChallenger orangeDodge = new DodgeChallenger("green", ", l/km", (byte)3,
+                (short)300, (short)492, 33333, false);
+        orangeDodge.getDescription();
+    }
+}
